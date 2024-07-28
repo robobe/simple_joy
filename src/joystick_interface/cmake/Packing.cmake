cba974d1ec2fe1596a2c3642c944c24529c8a2d9
robo <robo2020@gmail.com>
2024-07-28 09:31:57 +0000

    cpack

--- src/joystick_interface/cmake/Packing.cmake
+++ src/joystick_interface/cmake/Packing.cmake
@@ -13,8 +13,7 @@ set(CPACK_VERBATIM_VARIABLES YES)
 set(CPACK_PACKAGE_INSTALL_DIRECTORY ${CPACK_PACKAGE_NAME})
 SET(CPACK_OUTPUT_FILE_PREFIX "${CMAKE_SOURCE_DIR}/_packages")
 
-# https://unix.stackexchange.com/a/11552/254512
-set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/some")#/${CMAKE_PROJECT_VERSION}")
+
 
 set(CPACK_PACKAGE_VERSION_MAJOR ${PROJECT_VERSION_MAJOR})
 set(CPACK_PACKAGE_VERSION_MINOR ${PROJECT_VERSION_MINOR})
@@ -24,16 +23,17 @@ set(CPACK_PACKAGE_CONTACT "YOUR@dev.com")
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "YOUR NAME")
 
 # set(CPACK_RESOURCE_FILE_LICENSE "${CMAKE_CURRENT_SOURCE_DIR}/LICENSE")
+set(CPACK_PACKAGING_INSTALL_PREFIX "/opt/ros/humble")
 set(CPACK_RESOURCE_FILE_README "${CMAKE_CURRENT_SOURCE_DIR}/README.md")
+SET(CMAKE_SYSTEM_NAME Linux)
+if(${JETSON})
+    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE arm64)
+else()
+    SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE x64)
+endif()
 
-# package name for deb. If set, then instead of some-application-0.9.2-Linux.deb
-# you'll get some-application_0.9.2_amd64.deb (note the underscores too)
-# set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
-# that is if you want every group to have its own package,
-# although the same will happen if this is not set (so it defaults to ONE_PER_GROUP)
-# and CPACK_DEB_COMPONENT_INSTALL is set to YES
-# set(CPACK_COMPONENTS_GROUPING ALL_COMPONENTS_IN_ONE)#ONE_PER_GROUP)
-# without this you won't be able to pack only specified component
-# set(CPACK_DEB_COMPONENT_INSTALL YES)
+
+set(CPACK_DEBIAN_FILE_NAME DEB-DEFAULT)
+set(CPACK_GENERATOR "DEB")
 
 include(CPack)
